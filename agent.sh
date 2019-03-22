e9fe05f7591aef313509f6602419868b6c61ee02
Morten <mortenbjerke95@gmail.com>
2019-03-22 16:19:40 +0100

    bug fixes

--- agent.sh
+++ agent.sh
@@ -2,9 +2,9 @@
 
 sudo wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
 echo "downloaded puppet5"
-sudo dpkg -i puppetlabs-release-xenial.deb
+sudo dpkg -i puppet5-release-xenial.deb
 echo "packaged puppet5"
 sudo apt-get update
 sudo apt-get install puppet-agent
 echo "installed puppet-agent"
-sudo PATH=$PATH:/opt/puppetlabs/bin/
+sudo -i PATH=$PATH:/opt/puppetlabs/bin/
--- manager.sh
+++ manager.sh
@@ -2,10 +2,10 @@
 
 sudo wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
 echo "downloaded puppet5"
-sudo dpkg -i puppetlabs-release-xenial.deb
+sudo dpkg -i puppet5-release-xenial.deb
 echo "packaged puppet"
 sudo apt-get update
 sudo apt-get install puppetserver
 echo "installed puppetserver"
-sudo PATH=$PATH:/opt/puppetlabs/bin/
+sudo -i PATH=$PATH:/opt/puppetlabs/bin/
 #VIRKER IKKE WTF
